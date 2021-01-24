dfcc25e6ec32ec9f5e761e59603ad4e6593da7fc
liuding <liuding.ld@foxmail.com>
2021-01-24 23:19:13 +0800

    login

---
+++ src/views/login/Login.vue
@@ -0,0 +1,44 @@
+<template>
+  <ion-page>
+    <ion-content :fullscreen="true">
+      <ion-img src=""></ion-img>
+      <ion-button expand="block">微信登录</ion-button>
+
+      <ion-button>账号密码登录</ion-button>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import {
+  IonButton,
+  IonContent,
+  IonPage,
+} from "@ionic/vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Home",
+  data() {
+    return {
+    };
+  },
+  setup() {
+      return {};
+  },
+  components: {
+    IonContent,
+    IonPage,
+    IonButton,
+  },
+  methods: {
+  },
+});
+</script>
+
+<style scoped>
+ion-content {
+    --padding-start: 16px;
+    --padding-end: 16px;
+}
+</style>
---
+++ src/views/login/LoginWithPassword.vue
@@ -0,0 +1,127 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-buttons>
+          <ion-back-button
+            :text="getBackButtonText()"
+            default-href="/"
+          ></ion-back-button>
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content :fullscreen="true" v-if="message">
+    
+      <ion-item>
+          <ion-icon :icon="personCircle" color="primary"></ion-icon>
+        <ion-label>
+
+        </ion-label>
+        <ion-input placeholder="手机号"></ion-input>
+      </ion-item>
+
+       <ion-item>
+        <ion-label>密码</ion-label>
+        <ion-input></ion-input>
+      </ion-item>
+
+      <ion-button expand="block">登录</ion-button>
+
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import {
+  IonBackButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonPage,
+  IonToolbar,
+} from "@ionic/vue";
+import { personCircle } from "ionicons/icons";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Home",
+  data() {
+    return {
+      personCircle,
+      getBackButtonText: () => {
+        const win = window as any;
+        const mode = win && win.Ionic && win.Ionic.mode;
+        return mode === "ios" ? "Inbox" : "";
+      },
+    };
+  },
+  setup() {
+    const message = {
+      fromName: "Jordan Firth",
+      subject: "Report Results",
+      date: "4:55 AM",
+      id: 2,
+    };
+
+    return { message };
+  },
+  components: {
+    IonBackButton,
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonIcon,
+    IonItem,
+    IonLabel,
+    IonPage,
+    IonToolbar,
+  },
+});
+</script>
+
+<style scoped>
+ion-item {
+  --inner-padding-end: 0;
+  --background: transparent;
+}
+
+ion-label {
+  margin-top: 12px;
+  margin-bottom: 12px;
+}
+
+ion-item h2 {
+  font-weight: 600;
+}
+
+ion-item .date {
+  float: right;
+  align-items: center;
+  display: flex;
+}
+
+ion-item ion-icon {
+  font-size: 42px;
+  margin-right: 8px;
+}
+
+ion-item ion-note {
+  font-size: 15px;
+  margin-right: 12px;
+  font-weight: normal;
+}
+
+h1 {
+  margin: 0;
+  font-weight: bold;
+  font-size: 22px;
+}
+
+p {
+  line-height: 22px;
+}
+</style>
