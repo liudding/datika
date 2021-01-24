a88c73089f0ff2b39a1db4da374e85b12478beee
liuding <liuding.ld@foxmail.com>
2021-01-25 00:05:59 +0800

    router

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,63 +10,61 @@ const router = createRouter({
 
 
 function isLoggedIn(): boolean {
-    return false;
+  return true;
 }
 
 // permission judge function
 function hasPermission(rolesRequired: string[]) {
-    const roles: string[] = []; // user roles;
+  const roles: string[] = []; // user roles;
 
-    if (!rolesRequired || roles.includes('admin')) return true;
+  if (!rolesRequired || roles.includes('admin')) return true;
 
-    return roles.some(role => rolesRequired.indexOf(role) >= 0)
+  return roles.some(role => rolesRequired.indexOf(role) >= 0)
 }
 
 const whiteList = ['/login', '/login/password', '/auth-redirect'] // no redirect whitelist
 
 function isInWhitelist(to: RouteLocationNormalized) {
-    return whiteList.indexOf(to.path) !== -1;
+  return whiteList.indexOf(to.path) !== -1;
 }
 
 function needLogin(to: RouteLocationNormalized) {
-    if (isInWhitelist(to)) {
-        return false
-    }
+  if (isInWhitelist(to)) {
+    return false
+  }
 
-    if (!to.meta) {
-        return true;
-    }
+  if (!to.meta) {
+    return true;
+  }
 
-    if (to.meta.login === undefined || to.meta.login === null) {
-        // 如果未设置 “需要登录”，则默认为必须登录
-        return true
-    }
+  if (to.meta.login === undefined || to.meta.login === null) {
+    // 如果未设置 “需要登录”，则默认为必须登录
+    return true
+  }
 
-    return to.meta.login;
+  return to.meta.login;
 }
 
 router.beforeEach((to, from, next) => {
-    if (!needLogin(to)) {
-        next()
-        return
-    }
+  if (!isLoggedIn() && !needLogin(to)) {
+    next()
+    return
+  }
+  if (!isLoggedIn()) {
+    next({ path: '/login', replace: true })
+    return;
+  }
 
-    if (!isLoggedIn()) {
-        next({ path: '/login', replace: true })
-        return;
-    }
+  if (to.path.indexOf('/login') === 0) {
+    next({ path: '/' })
+    return;
+  }
 
-
-    if (to.path === '/login') {
-        next({ path: '/' })
-        return;
-    }
-
-    if (hasPermission(to.meta.roles)) {
-        next()
-    } else {
-        next({ path: '/401', replace: true })
-    }
+  if (hasPermission(to.meta.roles)) {
+    next()
+  } else {
+    next({ path: '/401', replace: true })
+  }
 
 })
 
