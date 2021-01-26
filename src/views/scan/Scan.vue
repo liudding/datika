dc01e3588233ecab356788c681ee74189f2a9ae1
ding <liuding.ld@foxmail.com>
2021-01-26 17:56:19 +0800

    scan page

--- src/views/scan/Scan.vue
+++ src/views/scan/Scan.vue
@@ -3,10 +3,7 @@
     <ion-header>
       <ion-toolbar>
         <ion-buttons>
-          <ion-back-button
-            text="back"
-            default-href="/"
-          ></ion-back-button>
+          <ion-back-button text="back" default-href="/"></ion-back-button>
         </ion-buttons>
       </ion-toolbar>
     </ion-header>
@@ -14,16 +11,29 @@
     <div id="camera-container"></div>
 
     <ion-content :fullscreen="false">
-     
-    </ion-content>
 
-    <ion-footer class="ion-no-border">
-    <ion-toolbar>
-      <ion-title>
-        <ion-button @click="openModal">Open Modal</ion-button>
-      </ion-title>
-    </ion-toolbar>
-  </ion-footer>
+       <ion-fab vertical="bottom" horizontal="end">
+        <ion-fab-button @click="gotoScan" translucent>
+          <ion-icon :icon="appsOutline"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
+
+      <div class="panel show" v-if="false">
+        <div class="panel-center"></div>
+        <div class="record">
+          <div>张无忌</div>
+          <div style="margin-top: 8px;">
+            <span style="font-size: 36px;">20</span>
+            <span style="font-size:10px; margin-left:3px;color:dark;">分</span>
+          </div>
+
+          <div style="color: gray;margin-top: 16px;">
+            <small>正确：</small>
+            <small>错误：</small>
+          </div>
+        </div>
+      </div>
+    </ion-content>
   </ion-page>
 </template>
 
@@ -36,13 +46,13 @@ import {
   IonHeader,
   IonPage,
   IonToolbar,
-  modalController
+  modalController,
 } from "@ionic/vue";
-import { personCircle } from "ionicons/icons";
+import { appsOutline } from "ionicons/icons";
 import { defineComponent } from "vue";
 
 import Scanner from "@/services/gradecam/Scanner";
-import Modal from './Modal.vue'
+import Modal from "./Modal.vue";
 
 let scanner: Scanner;
 
@@ -51,7 +61,7 @@ export default defineComponent({
   data() {
     return {
       // scanner: Scanner,
-      personCircle,
+      appsOutline,
       getBackButtonText: () => {
         const win = window as any;
         const mode = win && win.Ionic && win.Ionic.mode;
@@ -75,14 +85,13 @@ export default defineComponent({
     IonContent,
     IonHeader,
     IonPage,
-    IonToolbar
+    IonToolbar,
   },
   mounted() {
-    console.log('====')
+    console.log("====");
     this.initScanner();
   },
   ionViewWillLeave() {
-    console.log(' page will leave');
     scanner.stop();
   },
   methods: {
@@ -105,15 +114,14 @@ export default defineComponent({
     },
 
     async openModal() {
-      const modal = await modalController
-        .create({
-          component: Modal,
-          cssClass: 'my-custom-class',
-          componentProps: {
-            title: 'New Title',
-            swipeToClose: true
-          },
-        })
+      const modal = await modalController.create({
+        component: Modal,
+        cssClass: "my-custom-class",
+        componentProps: {
+          title: "New Title",
+          swipeToClose: true,
+        },
+      });
       return modal.present();
     },
 
@@ -160,51 +168,47 @@ ion-toolbar {
   --background: transparent !important;
   --background-color: transparent;
   --ion-color-base: transparent !important;
+  --border-width: 0px;
+  --border-color: transparent;
 }
 
-ion-item {
-  --inner-padding-end: 0;
-  --background: transparent;
-}
-
-ion-label {
-  margin-top: 12px;
-  margin-bottom: 12px;
-}
-
-ion-item h2 {
-  font-weight: 600;
-}
-
-ion-item .date {
-  float: right;
-  align-items: center;
-  display: flex;
-}
-
-ion-item ion-icon {
-  font-size: 42px;
-  margin-right: 8px;
-}
-
-ion-item ion-note {
-  font-size: 15px;
-  margin-right: 12px;
-  font-weight: normal;
-}
-
-h1 {
-  margin: 0;
-  font-weight: bold;
-  font-size: 22px;
-}
-
-p {
-  line-height: 22px;
+ion-fab-button {
+  --color: white;
+  --background: rgba(255, 255, 255, 0.3);
+  --background-activated: rgba(255, 255, 255, 0.5);
 }
 
 #camera-container {
   height: 100%;
   width: 100%;
+}
+
+.panel {
+  position: fixed;
+  z-index: 1000000000000;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: white;
+  height: 150px;
+  border-top-left-radius: 30px;
+  border-top-right-radius: 30px;
+
+  box-shadow: 0 -5px 5px rgba(0, 0, 0, 0.1);
+
+  padding: 8px;
+  display: flex;
+  /* align-items: center; */
+  justify-content: center;
+}
+
+.panel.show {
+  /* bottom: -100px; */
+}
+
+.record {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
