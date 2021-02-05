e3f6ce5e6a0246084e9a624efc8158909421df98
ding <liuding.ld@foxmail.com>
2021-02-05 16:36:03 +0800

    ios app

---
+++ ios/App/App/Unified-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Unified-Bridging-Header.h
+//  App
+//
+//  Created by ding on 2021/2/2.
+//
+
+#ifndef Unified_Bridging_Header_h
+#define Unified_Bridging_Header_h
+
+#import "cordova-plugin-iosrtc-Bridging-Header.h"
+
+#endif /* Unified_Bridging_Header_h */
