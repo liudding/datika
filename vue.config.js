88aa86c19d5413aa9616f7f416469054b4bdd4e4
liuding <liuding.ld@foxmail.com>
2021-02-10 00:02:04 +0800

    pwa

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,27 @@ module.exports = {
     devServer: {
         // https: true
     },
+
+    pwa: {
+        name: "Quiz",
+      
+        themeColor: "#ffffff",
+       
+        msTileColor: "#000000",
+        appleMobileWebAppStatusBarStyle: "default",
+        manifestOptions: {
+            "start_url": "./?source=pwa",
+            "description": "答题卡扫描",
+            "background_color": "red",
+            "orientation": "portrait-primary",
+        },
+        iconPaths: {
+            faviconSVG: 'img/icons/favicon.svg',
+            favicon32: 'img/icons/favicon-32x32.png',
+            favicon16: 'img/icons/favicon-16x16.png',
+            appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+            maskIcon: 'img/icons/safari-pinned-tab.svg',
+            msTileImage: 'img/icons/msapplication-icon-144x144.png'
+        }
+    }
 }
