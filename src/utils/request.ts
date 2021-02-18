f168967e8fb516f70618d0b6dad3e3c2af9c492e
ding <liuding.ld@foxmail.com>
2021-02-18 13:12:28 +0800

    request util

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import store from '@/store'
 
 const instance = axios.create({
     baseURL: process.env.VUE_APP_BASE_API_URL,
@@ -8,11 +9,49 @@ const instance = axios.create({
 instance.interceptors.request.use(config => {
     const token = localStorage.getItem('token');
     if (token) {
-        // 让每个请求携带token
         config.headers['Authorization'] = 'Bearer ' + token
-      }
-      return config
+    }
+
+    return config
 })
+
+instance.interceptors.response.use(
+    response => response,
+    error => {
+        console.log('[HTTP REQUEST ERROR]: ' + error, error.response);
+
+        if (error.response) {
+            // The request was made and the server responded with a status code
+
+            const response = error.response
+
+            if (response.status === 401) {
+                // 未认证
+                store.dispatch('feLogout').then(() => {
+                    // location.reload()
+                })
+                return Promise.reject(error)
+            }
+
+            if (response.data.message === 'Unauthenticated.') {
+                // 由于服务端在 token 失效时，会返回 500, 所以使用此 trick 来判断登录态是否有效
+
+                store.dispatch('feLogout').then(() => {
+                    location.reload()
+                })
+            }
+
+
+
+        } else if (error.request) {
+            // The request was made but no response was received
+        } else {
+            // Something happened in setting up the request that triggered an Error
+        }
+
+        return Promise.reject(error)
+    }
+)
 
 
 export default instance;
