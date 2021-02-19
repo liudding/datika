0df620c306cf50a1da9bc90c85100f8efa886a27
ding <liuding.ld@foxmail.com>
2021-02-19 10:51:50 +0800

    persistedstate

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex'
+import createPersistedState from "vuex-persistedstate";
 import classroom from './modules/classroom'
 import quiz from './modules/quiz'
 import user from './modules/user'
@@ -10,5 +11,8 @@ export default createStore({
         quiz,
         user
     },
+    plugins: [createPersistedState({
+        key: 'APP_STATE_DATA'
+    })],
     strict: process.env.NODE_ENV !== 'production'
 })
