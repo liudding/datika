ee6c575284ea0cac85088d1e3dd450a59a9f5c9b
ding <liuding.ld@foxmail.com>
2021-02-26 18:00:36 +0800

    result label

--- src/views/scan/Result.vue
+++ src/views/scan/Result.vue
@@ -8,15 +8,17 @@
         <b>{{ result.name }} </b
         ><span style="margin-left: 8px">{{ result.number }}</span>
       </div>
-      <div style="margin-top: 8px">
+      <div style="margin-top: 8px;">
         <span style="font-size: 36px">{{ result.score }}</span>
         <span style="font-size: 10px; margin-left: 3px; color: dark">分</span>
       </div>
 
+      <div class="result-label">{{label}}</div>
+<!-- 
       <div style="color: gray; margin-top: 16px">
-        <!-- <small>正确：</small>
-            <small>错误：</small> -->
-      </div>
+        <small>正确：</small>
+        <small>错误：</small>
+      </div> -->
     </div>
   </div>
 </template>
@@ -28,6 +30,16 @@ export default defineComponent({
   name: "Records",
   props: {
     result: Object,
+  },
+  computed: {
+    label() {
+      const map = {
+        nochange: '重复录入',
+        update: '更新'
+      }
+
+      return map[this.result.type];
+    }
   },
   data() {
     return {};
@@ -64,5 +76,16 @@ export default defineComponent({
   flex-direction: column;
   align-items: center;
   padding: 16px;
+  position: relative;
+}
+
+.result-label {
+  position: absolute;
+  top: 20px;
+  right: 0px;
+  transform: rotate(45deg);
+
+  color: red;
+  font-size: 14px;
 }
 </style>
