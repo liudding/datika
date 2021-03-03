8a8a74918e140202409a4fec2d06435923ad3fe4
ding <liuding.ld@foxmail.com>
2021-03-03 15:13:49 +0800

    about page

--- src/views/settings/About.vue
+++ src/views/settings/About.vue
@@ -8,32 +8,68 @@
         <ion-title>关于</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content :fullscreen="true"> </ion-content>
+    <ion-content :fullscreen="true">
+      <div class="main">
+        <img
+          @click="onClickLogo"
+          src="/img/icons/android-chrome-512x512.png"
+          class="logo"
+        />
+        <div>{{ appName }}</div>
+
+        <div class="version">{{version}}</div>
+      </div>
+    </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-export default {
-  name: "My",
+import { defineComponent } from "vue";
+import Console from "@/services/console";
+
+export default defineComponent({
   components: {},
   data() {
     return {
-      user: {
-        name: "ClassroomItem",
-      },
+      appName: process.env.VUE_APP_NAME,
+
+      version: '1.0.0',
+
+      clickCount: 0,
     };
   },
-};
+  methods: {
+    onClickLogo() {
+      this.clickCount++;
+
+      if (this.clickCount >= 12) {
+        Console.load();
+      }
+    },
+  },
+});
 </script>
 
 <style scoped>
-.profile ion-item {
-  /* height: 80px; */
-  --padding-top: 16px;
-  --padding-bottom: 16px;
+.main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  padding: 16px;
 }
-.profile .name {
-  margin-left: 8px;
-  font-weight: 700;
+
+.logo {
+  width: 80px;
+  height: 80px;
+  /* background-color: rgb(255, 183, 183); */
+  border-radius: 8px;
+  border: none;
+}
+
+.version {
+  margin-top: 4px;
+  color: gray;
+  font-size: 14px;
 }
 </style>
