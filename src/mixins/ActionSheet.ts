3a947d67205521beb533ddbe7da7bbe0df3e44f3
ding <liuding.ld@foxmail.com>
2021-03-04 21:08:57 +0800

    alert & actionSheet native

--- src/mixins/ActionSheet.ts
+++ src/mixins/ActionSheet.ts
@@ -1,9 +1,13 @@
 import { actionSheetController } from "@ionic/vue";
+import { Plugins, ActionSheetOptionStyle } from "@capacitor/core";
+const { Modals } = Plugins;
+
+import { isApp } from "@/utils/env";
 
 export default {
     methods: {
         async showActionSheet(options: any) {
-            const buttons = options.buttons;
+            const buttons = options.buttons || [];
 
             if (options.cancel) {
                 buttons.push({
@@ -12,13 +16,39 @@ export default {
                 })
             }
 
+            if (isApp()) {
+                const styles: any = {
+                    'destructive': ActionSheetOptionStyle.Destructive,
+                    'cancel': ActionSheetOptionStyle.Cancel
+                };
+
+                const actions = buttons.map((i: any) => {
+                    return {
+                        title: i.text || i.title,
+                        style: styles[i.role] || ActionSheetOptionStyle.Default
+                    };
+                });
+
+                const result = await Modals.showActions({
+                    title: options.header || options.title,
+                    message: options.subHeader || options.message,
+                    options: actions
+                })
+
+                const action = buttons[result.index];
+                action && action.handler && action.handler();
+
+                return;
+            }
+
+
             const actionSheet = await actionSheetController.create({
                 header: options.header || options.title,
                 subHeader: options.subHeader || options.message,
                 cssClass: options.cssClass,
                 buttons,
             });
-            
+
             return actionSheet.present();
         }
     }
--- src/mixins/Alert.ts
+++ src/mixins/Alert.ts
@@ -1,44 +1,68 @@
 import { alertController } from "@ionic/vue";
+import { Plugins } from "@capacitor/core";
+const { Modals } = Plugins;
+
+import { isApp } from "@/utils/env";
 
 export default {
     methods: {
         async alert(options: any) {
 
+
+
             return new Promise<void>((resolve, reject) => {
-                let buttons = options.buttons;
 
-                if (!options.buttons) {
-                    buttons = [];
-                    if (options.cancel) {
-                        buttons.push({
-                            text: options.cancelText || "取消",
-                            role: "cancel",
-                            cssClass: "secondary",
-                            handler: () => {
-                                reject();
-                            }
-                        })
-                    }
+                const buttons = options.buttons || [];
 
-                    const confirm = options.confirm === null || options.confirm === undefined ? true : options.confirm
-                    
-                    if (confirm) {
-                        buttons.push({
-                            text: options.confirmText || "确定",
-                            handler: () => {
-                                resolve()
-                            }
-                        })
-                    }
+                if (options.cancel) {
+                    buttons.push({
+                        text: options.cancelText || "取消",
+                        role: "cancel",
+                        cssClass: "secondary",
+                        handler: () => {
+                            reject();
+                        }
+                    })
                 }
 
-                alertController.create({
-                    header: options.header || options.title,
-                    message: options.message || options.msg,
-                    buttons,
-                }).then((alert) => {
-                    alert.present();
-                });
+                if (options.confirm || buttons.length === 0) {
+                    options.confirm = true;
+
+                    buttons.push({
+                        text: options.confirmText || "确定",
+                        handler: () => {
+                            resolve()
+                        }
+                    })
+                }
+
+
+                if (isApp()) {
+                    if (buttons.length === 1) {
+                        Modals.alert({
+                            title: options.header || options.title,
+                            message: options.message || options.msg,
+                            buttonTitle: options.confirmText || options.cancelText || "确定"
+                        }).then(() => {
+                            resolve();
+                        });
+                    } else if (buttons.length === 2 && options.confirm && options.cancel) {
+                        Modals.confirm({
+                            title: options.header || options.title,
+                            message: options.message || options.msg,
+                        }).then((result: any) => {
+                            result ? resolve() : reject();
+                        });
+                    }
+                } else {
+                    alertController.create({
+                        header: options.header || options.title,
+                        message: options.message || options.msg,
+                        buttons,
+                    }).then((alert) => {
+                        alert.present();
+                    });
+                }
             });
         }
     }
