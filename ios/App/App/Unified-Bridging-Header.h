523c1fae157e6db85d874170dfa390b6fdcb6f8b
ding <liuding.ld@foxmail.com>
2021-03-09 13:49:13 +0800

    wechat plugin

--- ios/App/App/Unified-Bridging-Header.h
+++ ios/App/App/Unified-Bridging-Header.h
@@ -8,6 +8,6 @@
 #ifndef Unified_Bridging_Header_h
 #define Unified_Bridging_Header_h
 
-#import "cordova-plugin-iosrtc-Bridging-Header.h"
+//#import "cordova-plugin-iosrtc-Bridging-Header.h"
 
 #endif /* Unified_Bridging_Header_h */
