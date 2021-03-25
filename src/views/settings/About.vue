6a9899d9ee1b429648a848f470781433104324cd
ding <liuding.ld@foxmail.com>
2021-03-25 13:27:52 +0800

    about new version

--- src/views/settings/About.vue
+++ src/views/settings/About.vue
@@ -18,6 +18,16 @@
         <div>{{ appName }}</div>
 
         <div class="version">{{ nativeVersion }} ({{ version }})</div>
+
+        <div
+          v-if="newVersionInfo && newVersionInfo.installedPath"
+          class="new-version"
+        >
+          <div class="version-num">
+            {{ newVersionInfo.version }}<span class="tag">新版本</span>
+          </div>
+          <small>重新启动后，将应用新版本</small>
+        </div>
       </div>
     </ion-content>
   </ion-page>
@@ -27,6 +37,7 @@
 import { defineComponent } from "vue";
 import Console from "@/services/console";
 import Storage from "@/utils/storage";
+import UpdateManager from "@/services/updater";
 import { Plugins } from "@capacitor/core";
 const { Device } = Plugins;
 
@@ -40,6 +51,8 @@ export default defineComponent({
 
       nativeVersion: "",
 
+      newVersionInfo: null,
+
       clickCount: 0,
     };
   },
@@ -51,6 +64,8 @@ export default defineComponent({
     const webVersion = await Storage.get("web_version");
 
     this.version = webVersion || process.env.VUE_APP_VERSION;
+
+    this.newVersionInfo = await UpdateManager.getNewVersion();
   },
   methods: {
     onClickLogo() {
@@ -64,19 +79,18 @@ export default defineComponent({
 });
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .main {
   display: flex;
   flex-direction: column;
   align-items: center;
-
+  text-align: center;
   padding: 16px;
 }
 
 .logo {
   width: 80px;
   height: 80px;
-  /* background-color: rgb(255, 183, 183); */
   border-radius: 8px;
   border: none;
 }
@@ -85,5 +99,28 @@ export default defineComponent({
   margin-top: 4px;
   color: gray;
   font-size: 14px;
+  text-align: center;
+}
+
+.new-version {
+  margin-top: 16px;
+  text-align: center;
+
+  .version-num {
+    font-size: 14px;
+  }
+
+  .tag {
+    margin-left: 4px;
+    border-radius: 4px;
+    border: 1px solid green;
+    color: green;
+    font-size: 10px;
+    padding: 2px;
+  }
+
+  small {
+    color: gray;
+  }
 }
 </style>
