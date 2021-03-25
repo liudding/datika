63042ee4500c4c18b4635589cd20527da4f089e5
ding <liuding.ld@foxmail.com>
2021-03-25 13:28:14 +0800

    console

--- src/services/console/index.ts
+++ src/services/console/index.ts
@@ -6,15 +6,19 @@ declare global {
 
 export default class Console {
 
-    static load() {
-        const url = '//cdn.jsdelivr.net/npm/eruda';
+    public static load(): void {
+        const url = 'https://cdn.jsdelivr.net/npm/eruda';
 
         const head = document.getElementsByTagName('HEAD').item(0);
         const scriptElement = document.createElement('script');
         scriptElement.type = 'text/javascript'
         scriptElement.src = url;
         scriptElement.onload = () => {
-            window.eruda.init();
+            window.eruda && window.eruda.init();
+        }
+
+        scriptElement.onerror = () => {
+            console.error('Console load failed')
         }
 
         head?.appendChild(scriptElement)
