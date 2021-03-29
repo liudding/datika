975b7bf3598de5c902c3c328733cb4395763468a
ding <liuding.ld@foxmail.com>
2021-03-29 21:32:43 +0800

    favicons

--- vue.config.js
+++ vue.config.js
@@ -35,7 +35,7 @@ module.exports = {
             faviconSVG: 'img/icons/favicon.svg',
             favicon32: 'img/icons/favicon-32x32.png',
             favicon16: 'img/icons/favicon-16x16.png',
-            appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+            appleTouchIcon: 'img/icons/apple-touch-icon.png',
             maskIcon: 'img/icons/safari-pinned-tab.svg',
             msTileImage: 'img/icons/msapplication-icon-144x144.png'
         }
